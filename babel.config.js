7a5f55522fce7d51d26c8ce57004fc3f50c7afee
R3nge <digo.rodrigomalaquias@hotmail.com>
2023-12-12 16:16:54 -0300

    subindo vercelll

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      '@babel/preset-react", "@babel/preset-env" ,"@babel/preset-typescript',
+      "@babel/preset-react",
+      "@babel/preset-env",
+      "@babel/preset-typescript",
     ],
   };
 };
